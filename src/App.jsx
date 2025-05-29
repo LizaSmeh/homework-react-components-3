12188bcfb8c569087450e0ceadfbed24238dcaa4
Elizaveta Smekhova <smekhovaelizaveta@gmail.com>
2025-05-29 14:59:38 +0300

    fix: добавлено состояние для кнопки результата

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,25 @@ function App() {
 	const [operand1, setOperand1] = useState('');
 	const [operand2, setOperand2] = useState('');
 	const [operator, setOperator] = useState('');
+	const [result, setResult] = useState('');
 
 	const onClear = () => {
 		setOperand1('');
 		setOperand2('');
 		setOperator('');
+		setResult('');
 	}
+
+
    return (
     <>
       <div className="calculator">
-    <div className="display">{operand1 + operator + operand2}</div>
+    <div className="display">{result === '' ? operand1 + operator + operand2 : result }</div>
     <div className="buttons">
         <button className="button btn-grey" onClick={onClear}>C</button>
 		<button className="button btn-pink" onClick={()=> {setOperator('+')}}>+</button>
 		<button className="button btn-pink" onClick={()=> {setOperator('-')}}>-</button>
-		<button className="button btn-pink" onClick={()=> eval(`${operand1} ${operator} ${operand2}`)}>=</button>
+		<button className="button btn-pink" onClick={() => operator === '+'? setResult(+operand1 + +operand2) : setResult(+operand1 - +operand2)}>=</button>
 
 		{NUMS.map((namber) => <button className="button" onClick={()=>{operator === ''?setOperand1(operand1 + namber):setOperand2(operand2 + namber)}}>{namber}</button> )}
 
