533d5c8e4b427d15a3c0328d373056f94b79c076
Elizaveta Smekhova <smekhovaelizaveta@gmail.com>
2025-05-29 19:24:38 +0300

    fix: исправлены обработчики

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,76 @@
+import { useState } from "react";
+import "./App.module.css";
+import styles from "./App.module.css";
 
-import { useState } from 'react'
-import './App.css'
-const NUMS = ['0', '1', '2', '3', '4','5', '6', '7', '8', '9']
+const NUMS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 function App() {
-	const [operand1, setOperand1] = useState('');
-	const [operand2, setOperand2] = useState('');
-	const [operator, setOperator] = useState('');
-	const [result, setResult] = useState('');
+	const [operand1, setOperand1] = useState("");
+	const [operand2, setOperand2] = useState("");
+	const [operator, setOperator] = useState("");
+	const [result, setResult] = useState("");
 
 	const onClear = () => {
-		setOperand1('');
-		setOperand2('');
-		setOperator('');
-		setResult('');
-	}
+		setOperand1("");
+		setOperand2("");
+		setOperator("");
+		setResult("");
+	};
 
+	const handleResult = () => {
+		switch (operator) {
+			case '+':
+				setResult(+operand1 + +operand2);
+				break;
+			case "-":
+				setResult(+operand1 - +operand2);
+				break;
+				default:
+				break;
+		}
+	};
 
-   return (
-    <>
-      <div className="calculator">
-    <div className="display">{result === '' ? operand1 + operator + operand2 : result }</div>
-    <div className="buttons">
-        <button className="button btn-grey" onClick={onClear}>C</button>
-		<button className="button btn-pink" onClick={()=> {setOperator('+')}}>+</button>
-		<button className="button btn-pink" onClick={()=> {setOperator('-')}}>-</button>
-		<button className="button btn-pink" onClick={() => operator === '+'? setResult(+operand1 + +operand2) : setResult(+operand1 - +operand2)}>=</button>
+	return (
+		<>
+			<div className={styles.calculator}>
+				<div className={styles.display}>
+					{result === ''? operand1 + operator + operand2 : result}
+				</div>
+				<div className={styles.buttons}>
+					<button className={styles["btn-grey"]} onClick={onClear}>
+						C
+					</button>
+					<button className={styles["btn-pink"]} onClick={()=> setOperator('+')}>
+						+
+					</button>
+					<button
+						className={styles["btn-pink"]}
+						onClick={()=> setOperator('-')}
+					>
+						-
+					</button>
+					<button
+						className={styles["btn-pink"]}
+						onClick={handleResult}
+					>
+						=
+					</button>
 
-		{NUMS.map((namber) => <button className="button" onClick={()=>{operator === ''?setOperand1(operand1 + namber):setOperand2(operand2 + namber)}}>{namber}</button> )}
-
-    </div>
-   </div>
-    </>
-  )
+					{NUMS.map((namber) => (
+						<button
+							className={styles.button}
+							onClick={() => {
+								operator === ""
+									? setOperand1(operand1 + namber)
+									: setOperand2(operand2 + namber);
+							}}
+						>
+							{namber}
+						</button>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
